caf5b0c2e5c29cd323e77b371839e6b0cafd32c6
benny <bianca.graczyk.lima@escola.pr.gov.br>
2025-05-26 21:26:58 +0000

    aula6

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Universo',
+    'Netuno completou apenas uma órbita ao redor do Sol?',
+    'Netuno leva cerca de 165 anos terrestres para completar uma órbita ao redor do Sol, então ele completou somente uma órbita desde sua descoberta em 1846.',
+)
+criaCartao(
+    'Universo',
+    'Naves espaciais visitaram todos os planetas do Sistema Solar?',
+    'Algumas naves espaciais visitaram todos os planetas do Sistema Solar, mas não todas. Por exemplo, a sonda Voyager 2 visitou Netuno e Urano, enquanto a sonda New Horizons sobrevoou Plutão.',
+)
+criaCartao(
+    'Universo',
+    'A Terra é o único planeta com água líquida?',
+    'Embora a Terra seja o único planeta conhecido com grandes quantidades de água líquida na superfície, há evidências de água líquida em outros corpos celestes, como luas de Júpiter e Saturno.',
+)
+criaCartao( 
+    'Universo',
+    'Há mais árvores na Terra do que estrelas na Via Láctea?',
+    'É verdade, há mais árvores na Terra do que estrelas na Via Láctea. Estima-se que existam cerca de 3 trilhões de árvores na Terra, enquanto a Via Láctea contém entre 100 e 400 bilhões de estrelas.',
+)
+criaCartao( 
+    'Universo',
+    'Existe chuva de vidro em algum planeta?',
+    ' Em HD 189733b, um exoplaneta a cerca de 64 anos-luz da Terra, há uma chuva de vidro que cai horizontalmente devido aos ventos extremamente fortes que sopram a mais de 7.000 km/h. Esse fenômeno demonstra a diversidade e a extrema variedade de condições atmosféricas que podem existir além da Terra.',
+criaCartao(
+    'Universo',
+    'Há um buraco negro em nossa galáxia?',
+    'Sim, o Sagittarius A*, um buraco negro que fica no baricentro da Via Láctea, sendo assim, está ao redor de onde todo restante da galáxia orbita. No entanto, cientistas apontam que não há perigo para as estrelas que ficam ao seu redor.',
+)
